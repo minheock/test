c27773ca742c84c48a9d34e855f51a2750c5eb9e
DESKTOP-K33JG8F\202-00000 <dygks0602@naver.com>
2023-06-22 10:53:32 +0900

    반복 수행하는 게임 만듦

--- game.py
+++ game.py
@@ -1,12 +1,12 @@
 import random 
 
 rn = random.randint(1,5)
+# gn = -1
 
-gn = int(input("랜덤 숫자를 추측해주세요: "))
+while gn != rn:    
+    gn = int(input("랜덤 숫자를 추측해주세요: "))
 
-while gn != rn:
     if rn == gn :
-        print("정답입니다.")
-        break
+        print('정답')
     else :
-        print(f"틀렸습니다. 정답은 {rn} 입니다.")
+        print('땡')    
