69824455fd17940226982bd9fcf5192c61cc6adf
DESKTOP-K33JG8F\202-00000 <dygks0602@naver.com>
2023-06-22 10:42:48 +0900

    반복문 추가

--- game.py
+++ game.py
@@ -4,10 +4,9 @@ rn = random.randint(1,5)
 
 gn = int(input("랜덤 숫자를 추측해주세요: "))
 
-gn = int(input("랜덤 숫자를 추측해봐이자식아: "))
-
-
-if rn == gn :
-    print("정답입니다.")
-else :
-    print(f"틀렸습니다. 정답은 {rn} 입니다.")
+while gn != rn:
+    if rn == gn :
+        print("정답입니다.")
+        break
+    else :
+        print(f"틀렸습니다. 정답은 {rn} 입니다.")
